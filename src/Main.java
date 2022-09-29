07799b76d06db6ec99e8eb329bf640008ed2a9ec
1E10 Laptop <sample@bths.edu>
2022-09-29 10:58:21 -0400

    .

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,33 @@ public class Main {
 
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your group? ");
-        String numOfPeople = scan.nextLine();
+        int numOfPeople = scan.nextInt();
         System.out.print("What's the tip percentage (0 - 100): ");
         int tipPercentage = scan.nextInt();
 
         double price = 0;
         double totalBill = 0;
 
-        while (price != -1) {
+        while (price != -1.0) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             price = scan.nextDouble();
-            if (price != -1);
-            totalBill += price;
+            if (price != -1){
+                totalBill += price;
+
+            }
+
+            scan.nextLine();
 
         }
 
         System.out.println("------------------------------");
-        System.out.println("Total Bill Before Tip: ");
+        System.out.println("Total Bill Before Tip: " + (formatter.format(totalBill)));
+        System.out.println("Tip Percentage: " + (formatter.format(tipPercentage)));
+        System.out.println("Total Tip: " + (formatter.format(totalBill*tipPercentage/100)));
+        System.out.println("Total Bill With Tip: " + (formatter.format(totalBill*tipPercentage/100 + totalBill)) );
+        System.out.println("Per Person Cost Before Tip: " + (formatter.format(totalBill/numOfPeople)));
+        System.out.println("Tip Per Person: " + (formatter.format(totalBill*tipPercentage/100/numOfPeople)));
+        System.out.println("Total Cost Per Person: " + (formatter.format((totalBill*tipPercentage/100 + totalBill)/numOfPeople)));
 
 
 
